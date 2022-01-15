a35abbba6e4a34c362093864161fd71dbc23951e
Pyzin Oleg <pzn.oleg@gmail.com>
2022-01-15 13:48:46 +0300

    Has been added variable.tf file and modified vm01.tf file.

---
+++ TF/variable.tf
@@ -0,0 +1,5 @@
+#
+variable "sshd-public-key" {
+   description = "Values of public key for use with sshd server on a VM side."
+   type = string
+}
--- TF/vm01.tf
+++ TF/vm01.tf
@@ -1,3 +1,9 @@
+# To login to the virtual mashine need to generate public and private key
+# use ssh-keygen to do it.
+# Public key place to the terraform.tfvars
+# with the form:
+# sshd-public-key="your public key which has been generated ssh-keygen"
+#
 resource "aws_instance" "vm-usa-01" {
   ami            = "ami-0fb653ca2d3203ac1"
   instance_type  = "t3.micro"
@@ -13,5 +19,5 @@ resource "aws_instance" "vm-usa-01" {
 
 resource "aws_key_pair" "ssh-vm-usa-01" {
   key_name = "admin"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCn8O9jM7Xb3YYdaUydP8isDcPtcWXS23yWCAqUjanGEFSwaqLD4VGs7tlh7s7q+Ogl0+1spfQs79VNyzL4+1qUznb6yQkaCcQYt4lXnYBWYDWtabHkxiO7XvhC0pZzLCEIw6DYKkH016Fyf4RFjMoo4X17PhKjLAwaAvmrHf/5bxaI2h5skt7vKVFLxyRI/qrV1zEm/EL8A3kWTqCP6Po6pGDDp7PXlEiQWV2HnJQ60hk67xfxiMhfBbCzLZIt9CDB9Ytkdt0VZTEuiDrnhcAB6PLcjSqa6kzbSwpIdb9Kd6hTkoq5HwB9mvWU9dKva2Sr4tkbZSCxMdwxibqgR7l3 vm-usa-01"
+  public_key = var.sshd-public-key 
 }
